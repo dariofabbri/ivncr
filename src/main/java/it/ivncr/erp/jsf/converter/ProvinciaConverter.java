ecbabf2df5ca78dc0fc373e1b0e4a8d5e27d5915
dariofabbri <dar.fabbri@gmail.com>
2013-02-15 09:22:01 +0100

    Trying to solve problems with autocomplete in provincia field.

---
+++ src/main/java/it/ivncr/erp/jsf/converter/ProvinciaConverter.java
@@ -0,0 +1,33 @@
+package it.ivncr.erp.jsf.converter;
+
+import it.ivncr.erp.model.generale.Provincia;
+import it.ivncr.erp.service.ServiceFactory;
+import it.ivncr.erp.service.lut.LUTService;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value="provinciaConverter")
+public class ProvinciaConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+
+		LUTService lutService = ServiceFactory.createService("LUT");
+
+		// Find specified item by sigla.
+		//
+		Provincia provincia = lutService.retrieveItemByDescrizione("Provincia", "sigla", value);
+		return provincia;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object object) {
+
+		Provincia provincia = (Provincia)object;
+		return provincia.getSigla();
+	}
+
+}
