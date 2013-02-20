63b2dbaf63f132fdc4cdf8faf8a8ba3803b156a7
dariofabbri <dar.fabbri@gmail.com>
2013-02-20 08:21:27 +0100

    Added test case for Primefaces forum.

---
+++ src/main/java/it/ivncr/erp/jsf/managedbean/test/RadioInDatatableTest.java
@@ -0,0 +1,134 @@
+package it.ivncr.erp.jsf.managedbean.test;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+
+@ManagedBean
+@ViewScoped
+public class RadioInDatatableTest implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private LazyDataModel<TestEntity> model;
+	private TestEntity selected;
+
+
+	// Used for preferred selection.
+	//
+	private List<TestEntity> currentListContent;
+	private Integer preferred;
+
+
+
+	private List<TestEntity> data;
+
+
+	public RadioInDatatableTest() {
+
+		// Initialize fake data.
+		//
+		data = new ArrayList<TestEntity>();
+		for(int i = 1; i <= 100; ++i) {
+
+			TestEntity te = new TestEntity();
+			te.setId(i);
+			te.setDescription(String.format("Item #%d", i));
+
+			data.add(te);
+		}
+
+
+		model = new LazyDataModel<TestEntity>() {
+
+			private static final long serialVersionUID = 1L;
+			@Override
+			public List<TestEntity> load(
+					int first,
+					int pageSize,
+					String sortField,
+					SortOrder sortOrder,
+					Map<String, String> filters) {
+
+				// Apply specified filter.
+				//
+				List<TestEntity> result = new ArrayList<TestEntity>();
+				String filter = filters.get("description");
+				for(TestEntity te : data) {
+
+					if(filter == null || te.getDescription().contains(filter)) {
+						result.add(te);
+					}
+				}
+
+				this.setRowCount(result.size());
+
+				currentListContent = result;
+				return result;
+			}
+
+			@Override
+			public Object getRowKey(TestEntity te) {
+
+				return te == null ? null : te.getId();
+			}
+
+			@Override
+			public TestEntity getRowData(String rowKey) {
+
+				int index = Integer.decode(rowKey) - 1;
+				return data.get(index);
+			}
+		};
+	}
+
+
+	public LazyDataModel<TestEntity> getModel() {
+		return model;
+	}
+
+	public void setModel(LazyDataModel<TestEntity> model) {
+		this.model = model;
+	}
+
+	public TestEntity getSelected() {
+		return selected;
+	}
+
+	public void setSelected(TestEntity selected) {
+		this.selected = selected;
+	}
+
+	public List<TestEntity> getCurrentListContent() {
+		return currentListContent;
+	}
+
+	public void setCurrentListContent(List<TestEntity> currentListContent) {
+		this.currentListContent = currentListContent;
+	}
+
+	public Integer getPreferred() {
+		return preferred;
+	}
+
+	public void setPreferred(Integer preferred) {
+		this.preferred = preferred;
+	}
+
+	public List<TestEntity> getData() {
+		return data;
+	}
+
+	public void setData(List<TestEntity> data) {
+		this.data = data;
+	}
+}
---
+++ src/main/java/it/ivncr/erp/jsf/managedbean/test/TestEntity.java
@@ -0,0 +1,23 @@
+package it.ivncr.erp.jsf.managedbean.test;
+
+public class TestEntity {
+
+	private Integer id;
+	private String description;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
