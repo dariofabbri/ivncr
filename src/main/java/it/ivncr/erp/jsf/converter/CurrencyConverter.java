13e74578e1f2be64423264509123745e8e9d992a
Dario Fabbri <dar.fabbri@gmail.com>
2013-03-19 10:51:07 +0100

    Fixed a problem with custom currency converter.

--- src/main/java/it/ivncr/erp/jsf/converter/CurrencyConverter.java
+++ src/main/java/it/ivncr/erp/jsf/converter/CurrencyConverter.java
@@ -3,44 +3,62 @@ package it.ivncr.erp.jsf.converter;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.ParseException;
+import java.util.regex.Pattern;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 @FacesConverter(value="currencyConverter")
 public class CurrencyConverter implements Converter {
 
-	private final DecimalFormat df;
+	private final Pattern pattern = Pattern.compile("^[0-9]*(,[0-9][0-9])?$", Pattern.CASE_INSENSITIVE);
+	private final DecimalFormat inputDf;
+	private final DecimalFormat outputDf;
 
 	public CurrencyConverter() {
 
 		DecimalFormatSymbols dfs = new DecimalFormatSymbols();
 		dfs.setDecimalSeparator(',');
 		dfs.setGroupingSeparator('.');
-		df = new DecimalFormat("0.##", dfs);
-		df.setGroupingUsed(false);
+
+		inputDf = new DecimalFormat("0.##", dfs);
+		inputDf.setGroupingUsed(false);
+
+		outputDf = new DecimalFormat("0.00", dfs);
+		outputDf.setGroupingUsed(false);
 	}
 
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
 
+		// If a regex pattern has been specified, apply it.
+		//
+		if(!pattern.matcher(value).matches()) {
+			FacesMessage message = new FacesMessage();
+			message.setSummary("Importo errato");
+			message.setDetail("L'importo immesso non è valido");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ConverterException(message);
+		}
+
 		try {
 
-			return df.parse(value);
+			return inputDf.parse(value);
 
 		} catch (ParseException e) {
 
-			throw new RuntimeException("Unable to convert specified string to currency: " + value);
+			throw new RuntimeException("Exception caught while parsing: " + value);
 		}
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component, Object object) {
 
-		return df.format(object);
+		return outputDf.format(object);
 
 	}
-
 }
