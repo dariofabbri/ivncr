5cb8241cce6d97568f24be0016907236488c847b
dariofabbri <dar.fabbri@gmail.com>
2013-04-23 07:51:22 +0200

    Further improvements to fatturazione tab of OdS detail page.

--- src/main/java/it/ivncr/erp/jsf/managedbean/commerciale/ods/DettaglioOdsFatturazione.java
+++ src/main/java/it/ivncr/erp/jsf/managedbean/commerciale/ods/DettaglioOdsFatturazione.java
@@ -1,17 +1,26 @@
 package it.ivncr.erp.jsf.managedbean.commerciale.ods;
 
 
+import it.ivncr.erp.jsf.RobustLazyDataModel;
+import it.ivncr.erp.jsf.managedbean.accesso.session.LoginInfo;
+import it.ivncr.erp.model.commerciale.cliente.Cliente;
 import it.ivncr.erp.model.commerciale.contratto.Canone;
 import it.ivncr.erp.model.commerciale.contratto.RaggruppamentoFatturazione;
 import it.ivncr.erp.model.commerciale.contratto.Tariffa;
 import it.ivncr.erp.model.commerciale.ods.OdsFrazionamento;
+import it.ivncr.erp.service.QueryResult;
 import it.ivncr.erp.service.ServiceFactory;
+import it.ivncr.erp.service.SortDirection;
+import it.ivncr.erp.service.cliente.ClienteService;
 import it.ivncr.erp.service.lut.LUTService;
 import it.ivncr.erp.service.odsfrazionamento.OdsFrazionamentoService;
 import it.ivncr.erp.service.ordineservizio.OrdineServizioService;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -21,6 +30,8 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +47,9 @@ public class DettaglioOdsFatturazione implements Serializable {
 	@ManagedProperty("#{dettaglioOdsGenerale}")
 	private DettaglioOdsGenerale dettaglioOdsGenerale;
 
+	@ManagedProperty("#{loginInfo}")
+    private LoginInfo loginInfo;
+
 	private Boolean oneroso;
 	private Integer codiceTariffa;
 	private Integer codiceCanone;
@@ -49,9 +63,62 @@ public class DettaglioOdsFatturazione implements Serializable {
 
 	private OdsFrazionamento selectedOdsFrazionamento;
 
+	private LazyDataModel<Cliente> clienteModel;
+	private Cliente selectedCliente;
+
+	private BigDecimal quota;
+	private Boolean esclusioneRitenutaGaranzia;
+
 
 	public DettaglioOdsFatturazione() {
+
+		clienteModel = new RobustLazyDataModel<Cliente>() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public List<Cliente> load(
+					int first,
+					int pageSize,
+					String sortField,
+					SortOrder sortOrder,
+					Map<String, String> filters) {
+
+				logger.debug("Fetching data model.");
+
+				// Inject codice azienda argument in applied filters map.
+				//
+				filters.put("codiceAzienda", Integer.toString(loginInfo.getCodiceAzienda()));
+
+				ClienteService cs = ServiceFactory.createService("Cliente");
+				QueryResult<Cliente> result = cs.list(
+						first,
+						pageSize,
+						sortField,
+						SortDirection.fromSortOrder(sortOrder),
+						filters);
+
+				this.setRowCount(result.getRecords());
+
+				return result.getResults();
+			}
+
+			@Override
+			public Object getRowKey(Cliente cliente) {
+
+				return cliente == null ? null : cliente.getId();
+			}
+
+			@Override
+			public Cliente getRowData(String rowKey) {
+
+				ClienteService cs = ServiceFactory.createService("Cliente");
+				Cliente cliente = cs.retrieve(Integer.decode(rowKey));
+				return cliente;
+			}
+		};
 	}
+
 
 	@PostConstruct
 	public void init() {
@@ -122,6 +189,71 @@ public class DettaglioOdsFatturazione implements Serializable {
 	}
 
 
+	public void startCreateFrazionamento() {
+
+		logger.debug("Entering startCreateFrazionamento() method.");
+
+		quota = null;
+		esclusioneRitenutaGaranzia = null;
+
+		selectedOdsFrazionamento = null;
+	}
+
+
+	public void startUpdateFrazionamento() {
+
+		logger.debug("Entering startUpdateFrazionamento() method.");
+
+		// No need to reload from DB, the table is an in memory snapshot that
+		// must be confirmed or canceled in a single shot.
+		//
+		selectedCliente = selectedOdsFrazionamento.getCliente();
+		quota = selectedOdsFrazionamento.getQuota();
+		esclusioneRitenutaGaranzia = selectedOdsFrazionamento.getEsclusioneRitenutaGaranzia();
+	}
+
+
+	public void doSaveFrazionamento() {
+
+		logger.debug("Entering doSaveFrazionamento() method.");
+
+		// Initialize in memory list, if needed.
+		//
+		if(listOdsFrazionamento == null) {
+			listOdsFrazionamento = new ArrayList<OdsFrazionamento>();
+		}
+
+		if(selectedOdsFrazionamento == null) {
+
+			OdsFrazionamento odsFrazionamento = new OdsFrazionamento();
+			odsFrazionamento.setCliente(selectedCliente);
+			odsFrazionamento.setQuota(quota);
+			odsFrazionamento.setEsclusioneRitenutaGaranzia(esclusioneRitenutaGaranzia);
+
+			// Find max id.
+			//
+			int maxId = 0;
+			for(OdsFrazionamento o : listOdsFrazionamento) {
+				if(o.getId() > maxId) {
+					maxId = o.getId();
+				}
+			}
+			odsFrazionamento.setId(maxId + 1);
+
+			listOdsFrazionamento.add(odsFrazionamento);
+
+		} else {
+
+			selectedOdsFrazionamento.setQuota(quota);
+			selectedOdsFrazionamento.setEsclusioneRitenutaGaranzia(esclusioneRitenutaGaranzia);
+		}
+
+		// Signal to modal dialog that everything went fine.
+		//
+		RequestContext.getCurrentInstance().addCallbackParam("ok", true);
+	}
+
+
 	private boolean formValidations() {
 
 		// At least one between canone and tariffa must be selected.
@@ -140,6 +272,15 @@ public class DettaglioOdsFatturazione implements Serializable {
 	public void setDettaglioOdsGenerale(DettaglioOdsGenerale dettaglioOdsGenerale) {
 		this.dettaglioOdsGenerale = dettaglioOdsGenerale;
 	}
+
+	public LoginInfo getLoginInfo() {
+		return loginInfo;
+	}
+
+	public void setLoginInfo(LoginInfo loginInfo) {
+		this.loginInfo = loginInfo;
+	}
+
 
 	public Boolean getOneroso() {
 		return oneroso;
@@ -222,5 +363,37 @@ public class DettaglioOdsFatturazione implements Serializable {
 	public void setSelectedOdsFrazionamento(
 			OdsFrazionamento selectedOdsFrazionamento) {
 		this.selectedOdsFrazionamento = selectedOdsFrazionamento;
+	}
+
+	public LazyDataModel<Cliente> getClienteModel() {
+		return clienteModel;
+	}
+
+	public void setClienteModel(LazyDataModel<Cliente> clienteModel) {
+		this.clienteModel = clienteModel;
+	}
+
+	public Cliente getSelectedCliente() {
+		return selectedCliente;
+	}
+
+	public void setSelectedCliente(Cliente selectedCliente) {
+		this.selectedCliente = selectedCliente;
+	}
+
+	public BigDecimal getQuota() {
+		return quota;
+	}
+
+	public void setQuota(BigDecimal quota) {
+		this.quota = quota;
+	}
+
+	public Boolean getEsclusioneRitenutaGaranzia() {
+		return esclusioneRitenutaGaranzia;
+	}
+
+	public void setEsclusioneRitenutaGaranzia(Boolean esclusioneRitenutaGaranzia) {
+		this.esclusioneRitenutaGaranzia = esclusioneRitenutaGaranzia;
 	}
 }
