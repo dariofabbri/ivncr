4c7b7805e37df55cc9b44198053010766fe09120
dariofabbri <dar.fabbri@gmail.com>
2013-05-12 19:46:01 +0200

    Menu status object made serializable.

--- src/main/java/it/ivncr/erp/jsf/managedbean/accesso/session/MenuBar.java
+++ src/main/java/it/ivncr/erp/jsf/managedbean/accesso/session/MenuBar.java
@@ -13,7 +13,6 @@ import org.apache.shiro.SecurityUtils;
 import org.primefaces.component.menuitem.MenuItem;
 import org.primefaces.component.separator.Separator;
 import org.primefaces.component.submenu.Submenu;
-import org.primefaces.model.DefaultMenuModel;
 import org.primefaces.model.MenuModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,11 +26,11 @@ import org.w3c.dom.NodeList;
 public class MenuBar implements Serializable {
 
 	private static final Logger logger = LoggerFactory.getLogger(MenuBar.class);
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private MenuModel menuModel;
-	
+
 	public MenuModel getMenuModel() {
 		return menuModel;
 	}
@@ -51,128 +50,128 @@ public class MenuBar implements Serializable {
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			doc = db.parse(is);
 		} catch(Exception e) {
-			
+
 			logger.error("Exception caught while parsing XML control file for menu definition.", e);
 			throw new RuntimeException("Exception caught while parsing XML control file for menu definition.", e);
 		}
-		
+
 		menuModel = buildMenuModel(doc);
 		logger.debug("Menu model successfully built.");
 	}
-	
+
 	private MenuModel buildMenuModel(Document doc) {
 
-		MenuModel model = new DefaultMenuModel();
-		
+		MenuModel model = new SerializableDefaultMenuModel();
+
 		NodeList nl = doc.getDocumentElement().getChildNodes();
 		for(int i = 0; i < nl.getLength(); ++i) {
-			
+
 			Node node = nl.item(i);
-			
+
 			if(node.getNodeType() != Node.ELEMENT_NODE)
 				continue;
-			
+
 			Element element = (Element)node;
-			
+
 			if(element.getNodeName().equals("menuitem")) {
-				
+
 				MenuItem item = buildMenuItem(element);
 				model.addMenuItem(item);
-				
+
 			} else if(element.getNodeName().equals("separator")) {
 
 				Separator separator = buildSeparator(element);
 				model.addSeparator(separator);
-				
+
 			} else if(element.getNodeName().equals("submenu")) {
 
 				Submenu submenu = buildSubmenu(element);
 				model.addSubmenu(submenu);
-				
+
 			}
 		}
-		
+
 		return model;
 	}
-	
-	
+
+
 	private Submenu buildSubmenu(Element element) {
-		
+
 		Submenu result = new Submenu();
-		
+
 		String label = element.getAttribute("label");
 		if(!StringUtils.isEmpty(label))
 			result.setLabel(label);
-		
+
 		String icon = element.getAttribute("icon");
 		if(!StringUtils.isEmpty(icon))
 			result.setIcon(icon);
-		
-		
+
+
 		NodeList nl = element.getChildNodes();
 		for(int i = 0; i < nl.getLength(); ++i) {
-			
+
 			Node node = nl.item(i);
-			
+
 			if(node.getNodeType() != Node.ELEMENT_NODE)
 				continue;
-			
+
 			Element child = (Element)node;
-			
+
 			if(child.getNodeName().equals("menuitem")) {
-				
+
 				MenuItem item = buildMenuItem(child);
 				result.getChildren().add(item);
-				
+
 			} else if(child.getNodeName().equals("separator")) {
 
 				Separator separator = buildSeparator(child);
 				result.getChildren().add(separator);
-				
+
 			} else if(child.getNodeName().equals("submenu")) {
 
 				Submenu submenu = buildSubmenu(child);
 				result.getChildren().add(submenu);
-				
+
 			}
 		}
-		
+
 		return result;
 	}
 
-	
+
 	private Separator buildSeparator(Element element) {
 
 		Separator separator = new Separator();
 		return separator;
 	}
 
-	
+
 	private MenuItem buildMenuItem(Element element) {
-		
+
 		MenuItem item = new MenuItem();
 
 		// Only a subset of possible attributes is supported.
 		//
-		
+
 		String value = element.getAttribute("value");
 		if(!StringUtils.isEmpty(value))
 			item.setValue(value);
-		
+
 		String url = element.getAttribute("url");
 		if(!StringUtils.isEmpty(url))
 			item.setUrl(url);
-		
+
 		String icon = element.getAttribute("icon");
 		if(!StringUtils.isEmpty(icon))
 			item.setIcon(icon);
-		
+
 		String permission = element.getAttribute("icon");
 		if(!StringUtils.isEmpty(permission)) {
 			boolean permitted = SecurityUtils.getSubject().isPermitted(permission);
 			item.setDisabled(!permitted);
 		}
-		
+
 		return item;
 	}
 }
---
+++ src/main/java/it/ivncr/erp/jsf/managedbean/accesso/session/SerializableDefaultMenuModel.java
@@ -0,0 +1,11 @@
+package it.ivncr.erp.jsf.managedbean.accesso.session;
+
+import java.io.Serializable;
+
+import org.primefaces.model.DefaultMenuModel;
+
+public class SerializableDefaultMenuModel extends DefaultMenuModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
