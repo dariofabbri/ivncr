71eeeb2c1f9e77ff3a32d4173b5b35d392c77d6e
Dario Fabbri <dar.fabbri@gmail.com>
2013-05-22 13:13:01 +0200

    Test for report generation optimization.

--- src/main/java/it/ivncr/erp/jsf/managedbean/test/PdfReportTest.java
+++ src/main/java/it/ivncr/erp/jsf/managedbean/test/PdfReportTest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.slf4j.Logger;
@@ -42,4 +43,59 @@ public class PdfReportTest implements Serializable {
 
 		return new DefaultStreamedContent(bais, "application/pdf");
 	}
+	
+	
+	private byte[] getReport(String name, Map<String, Object> parameters) {
+		
+		
+		
+	}
+	
+	public class ReportKey {
+		
+		private String name;
+		private Map<String, Object> parameters;
+		
+		public ReportKey() {
+		}
+
+		public ReportKey(String name, Map<String, Object> parameters) {
+			this.name = name;
+			this.parameters = parameters;
+		}
+		
+		@Override
+		public int hashCode() {
+			
+			HashCodeBuilder hcb = new HashCodeBuilder(11, 103);
+			
+			hcb.append(name);
+			
+			if(parameters != null) {
+				for(String key : parameters.keySet()) {
+					
+				}
+			}
+			
+			return hcb.toHashCode();
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Map<String, Object> getParameters() {
+			return parameters;
+		}
+
+		public void setParameters(Map<String, Object> parameters) {
+			this.parameters = parameters;
+		}
+	}
+	
+	
 }
